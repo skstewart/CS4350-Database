ced0243c67887d937635d13d422075f767aa52d9
Shayna Stewart <shaynakstewart@gmail.com>
2019-11-20 22:55:30 -0800

    Update TargutDatabase.java

--- src/TargutDatabase.java
+++ src/TargutDatabase.java
@@ -37,6 +37,12 @@ public class TargutDatabase {
             statement = connection.createStatement();
 
             // db.insertItem(connection); //   THIS IS A TEST TO INSERT AN ITEM
+            
+            // db.insertCustomer(connection); //   THIS IS A TEST TO INSERT A CUSTOMER
+            
+            // db.insertDepartment(connection); //   THIS IS A TEST TO INSERT A DEPARTMENT
+            
+            
             resultSet = statement.executeQuery("SELECT * FROM ITEM");
 
             // ----------------------- FORMAT THIS TO FIT THE UI: ---------------------------------------------------------
@@ -58,7 +64,7 @@ public class TargutDatabase {
             resultSet = statement.executeQuery("SELECT * FROM CUSTOMER");
 
             // ----------------------- FORMAT THIS TO FIT THE UI: ---------------------------------------------------------
-            System.out.println("ID\tCustomer ID\t Name\t Email\t Payment Method \tShipment Info\tConfirmation\tPhone\t");
+            System.out.println("ID\tCustomer ID\t Name\t Email\t Payment Method \tShipping Info\tConfirmation\tPhone\t");
             System.out.println("===\t========\t==================\t============\t=========\t=======\t=========\t===========");
 
             // this prints to console, configure it to print to the UI
@@ -108,13 +114,13 @@ public class TargutDatabase {
     // -------------------------------------INSERTION METHODS--------------------------------------------------------
     void insertCustomer(Connection conn) throws ClassNotFoundException, SQLException, IOException {
 
-        int custID = 99;
-        String name = "'abc'";          // !!!!!!!!!! make sure any user entered string values have single quotes attached to both ends. ie) abc would become 'abc'
-        String email = "'abc'";
-        String paymentMethod = "'abc'";
-        String shipInfo = "'abc'";
-        boolean confirmation = true;
-        int phone = 1234567;
+        int custID = 99; //have the user able to enter this value
+        String name = "'abc'"; // !!!!!!!!!! make sure any user entered string values have single quotes attached to both ends. ie) abc would become 'abc'
+        String email = "'abc'"; //have the user able to enter this value
+        String paymentMethod = "'abc'"; //have the user able to enter this value
+        String shipInfo = "'abc'"; //have the user able to enter this value
+        boolean confirmation = true; //have the user able to enter this value
+        int phone = 1234567; //have the user able to enter this value
 
         String stmt = "INSERT INTO Customer([Customer ID],[Cust Name],Email,[Payment Method],[Shipping Info],Confirmation,[Phone Number]) VALUES("
                 + custID + "," + name + "," + email + "," + paymentMethod + "," + shipInfo + "," + confirmation + "," + phone + ")";
@@ -126,9 +132,8 @@ public class TargutDatabase {
     }
 
     void insertDepartment(Connection conn) throws ClassNotFoundException, SQLException {
-        int deptID = 0;
-
-        String deptName = "'abc'";
+        int deptID = 0; //have the user able to enter this value
+        String deptName = "'abc'"; //have the user able to enter this value
 
         String stmt = "INSERT INTO Department([Dept ID],[Dept Name]) VALUES("
                 + deptID + "," + deptName + ")";
@@ -140,17 +145,12 @@ public class TargutDatabase {
     }
 
     void insertItem(Connection conn) throws ClassNotFoundException, SQLException {
-        int itemID = 0;
-
-        String itemName = "'abc'";
-
-        String expDate = "'1/1/2099'";
-
-        double price = 0;
-
-        int stock = 0;
-
-        boolean onSale = false;
+        int itemID = 0; //have the user able to enter this value
+        String itemName = "'abc'"; //have the user able to enter this value
+        String expDate = "'1/1/2099'"; //have the user able to enter this value
+        double price = 0; //have the user able to enter this value
+        int stock = 0; //have the user able to enter this value
+        boolean onSale = false; //have the user able to enter this value
 
         String stmt = "INSERT INTO Item([Item ID],[Item Name],[Exp Date],Price,Stock,[On Sale]) VALUES("
                 + itemID + "," + itemName + "," + expDate + "," + price + "," + stock + "," + onSale + ")";
@@ -164,10 +164,14 @@ public class TargutDatabase {
     // -------------------------------------------------MODIFICATION METHODS----------------------------------------------
     void modifyTable(Connection conn) throws ClassNotFoundException, SQLException {
         String tableToUpdate = "Customer";      //these are test cases, update them to 
-        String columnToUpdate = "Phone";
+        String columnToUpdate = "Shipping Info";
+        String condition = "[Name]";
+        String conditionValue = "'Cody'";
+        String newValue = "'abcdefg'"; //need to be able to enter either int, string, double, or boolean
 
         String stmt = "UPDATE " + tableToUpdate + "\n"
-                + "SET " + columnToUpdate + " = 0000000";
+                + "SET " + columnToUpdate + " = " + newValue
+                + " WHERE " + condition +  " = " + conditionValue ;
 
         Statement s = conn.createStatement();
 
